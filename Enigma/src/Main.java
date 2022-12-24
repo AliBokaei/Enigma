f930c85c940d3d94bf533ea602bca7761916ed17
HiD3N <HiD3N.CP@GMAIL.COM>
2022-12-25 02:56:59 +0330

    Rotator 1 to Completed

--- Enigma/src/Main.java
+++ Enigma/src/Main.java
@@ -6,12 +6,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+
 //        String inputStr = input.nextLine();
 //        char strChars[] = inputStr.toCharArray();
+
+
         Enigma enigma = new Enigma();
-        System.out.print("Please Enter the PlugBoard : ");
+
+        System.out.print("Please Enter the PlugBoard Like (AF,BM,GH,JC,XE,OP,NR,ZL) : ");
         String plugBoardStr =input.nextLine();
+        System.out.println();
         enigma.PlugBoard(plugBoardStr);
+        System.out.println();
+
+        System.out.print("Please Enter the Rotator1 Like (LUWJHIKDYCAXMNQBZTRFGESVPO) : ");
+        String rotate1Str =input.nextLine();
+        System.out.println();
+        enigma.Rotator1(rotate1Str);
+        System.out.println();
+
+        System.out.print("Please Enter the Rotator2 Like (QNGHSZAFEBJRLUCTXYIMPDWKOV) : ");
+        String rotate2Str =input.nextLine();
+        System.out.println();
+        enigma.Rotator2(rotate2Str);
+        System.out.println();
+
+        System.out.print("Please Enter the Rotator3 Like (CMFQSBHIOAKRTENZLDYWUGPJXV) : ");
+        String rotate3Str =input.nextLine();
+        System.out.println();
+        enigma.Rotator3(rotate3Str);
+        System.out.println();
+
+
+
+
 //        for (char c:strChars) {
 ////            System.out.println(c);
 //            System.out.println(enigma.EnigmaFindEncrypt(c));
@@ -23,7 +52,16 @@ public class Main {
 
 class Enigma{
 
+    int nRotate1 = 0; // (nRotate1+1)%26
+    int nRotate2 = 0;
+    int nRotate3 = 0;
+
+
     Map<Character, Character> PlugBoard = new HashMap<>();
+    Map<Character, Character> Rotator1 = new HashMap<>();
+    Map<Character, Character> Rotator2 = new HashMap<>();
+    Map<Character, Character> Rotator3 = new HashMap<>();
+
 
     public void PlugBoard(String plugBoardStr){
         String arrayplugBoardStr[] =  plugBoardStr.split(",");
@@ -35,8 +73,47 @@ class Enigma{
 
     }
 
-    public char EnigmaFindEncrypt(char c){
 
+    public void Rotator1(String rotate1Str){  //    ascii A to Z is 65 to  90
+        int asciNum = 65;
+        char c;
+        for (int i = 0; i < rotate1Str.length(); i++) {
+            c = (char) asciNum;
+            Rotator1.put(c, rotate1Str.charAt(i));
+            asciNum++;
+        }
+        System.out.println(Rotator1);
+
+    }
+
+    public void Rotator2(String rotate2Str){  //    ascii A to Z is 65 to  90
+        int asciNum = 65;
+        char c;
+        for (int i = 0; i < rotate2Str.length(); i++) {
+            c = (char) asciNum;
+            Rotator2.put(c, rotate2Str.charAt(i));
+            asciNum++;
+        }
+        System.out.println(Rotator2);
+
+    }
+
+
+    public void Rotator3(String rotate3Str){  //    ascii A to Z is 65 to  90
+        int asciNum = 65;
+        char c;
+        for (int i = 0; i < rotate3Str.length(); i++) {
+            c = (char) asciNum;
+            Rotator3.put(c, rotate3Str.charAt(i));
+            asciNum++;
+        }
+        System.out.println(Rotator3);
+
+    }
+
+
+
+    public char EnigmaFindEncrypt(char c){
         return 'a';
     }
 
